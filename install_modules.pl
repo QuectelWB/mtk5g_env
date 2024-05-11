5ee0b3c5ec7678f5ef1ab83277ff40782b3caa45
MrVW <wang_biao@126.com>
2024-05-11 09:41:45 +0800

    install perl modules

---
+++ install_modules.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use CPAN;
+
+# 安装 Switch 模块
+CPAN::Shell->install('Switch');
+
+# 安装 File::Copy::Recursive 模块
+CPAN::Shell->install('File::Copy::Recursive');
+
+# 安装 XML::Simple 模块
+CPAN::Shell->install('XML::Simple');
+
